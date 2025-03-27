6c910f87b4fe2ef862d73f982630d0d57900f437
Hanzala Khattak <muhammadhanzalahkhan1@gmail.com>
2025-03-27 12:26:03 +0500

    Add HTML and JavaScript files for Conditional Statements demonstration

---
+++ Conditional Statements/script.js
@@ -0,0 +1,88 @@
+// let marks = prompt("Enter your marks: ");
+// console.log(marks, typeof(marks));
+
+// if(isNaN(marks)){
+//     console.log("Enter a valid number", marks);
+// }
+
+
+// ternay operator
+// let Number = 31; 
+// Number % 2 === 0 ? console.log(Number, " is Even"): console.log(Number, " is Odd");
+
+
+
+// let num1 = +prompt("Enter a number: ");
+// let num2 = +prompt("Enter another number: ");
+// let num3 = +prompt("Enter another number: ");
+// if(num1 > num2 && num1 > num3){
+//     console.log(num1, " is the largest number");
+// }else if(num2 > num1 && num2 > num3){
+//     console.log(num2, " is the largest number");    
+// }else if(num3 > num1 && num3 > num2){
+//     console.log(num3, " is the largest number");
+// }else{
+//     console.log("The numberss you entered are equal");
+// }
+
+
+
+// let num = +prompt("Enter a number: ");
+// switch (true) {
+//     case num < 0:
+//         console.log(num, "is a negative number");
+//         break;
+//     case num > 0:
+//         console.log(num, "is a positive number");
+//         break;
+//     case num === 0:
+//         console.log(num, "is zero");
+//         break;
+//     default:
+//         console.log("Enter a valid number");
+//         break;
+// }
+
+
+// let marks = +prompt("Enter your marks: ");
+// switch(true){
+//     case marks >= 90:
+//         console.log("A");
+//         break;
+//     case marks >= 80:
+//         console.log("B");
+//         break;  
+//     case marks >= 70:
+//         console.log("C");
+//         break;
+//     case marks >= 60:
+//         console.log("D");
+//         break;
+//     default:
+//         console.log("Fail");
+//         break;
+// }
+
+// let num = 35;
+// if(num % 3 === 0 && num % 5 === 0){
+//     console.log(num, " is divisible both by 3 and 5");
+// }else{
+//     console.log(num," is not divisible both by 3 and 5");
+// }
+
+
+// let myEmail = "m.hanzala2022@uop.edu.pk";
+// let myPass = 12345;
+// let email = prompt("Enter your email: ");
+// if(email === myEmail){
+//     console.log("The Email is correct");
+// }else{
+//     console.log("The Email is incorrect");
+// }
+
+// let pass = +prompt("Enter your password: ");
+// if(pass === myPass){
+//     console.log("Correct password");
+// }else{
+//     console.log("Incorrect password");
+// }
